fb0f03cea0e3929fa9b50e0011227400509e3b5d
Aaron Bailey <aaronthesnowboarder@live.com>
2022-02-19 04:27:52 -0800

    added chatroom menu to be open to closed. needs animation, but basic function is there

--- client/src/Components/Chatroom.js
+++ client/src/Components/Chatroom.js
@@ -3,31 +3,11 @@ import './Chatroom.css'
 import {
     Card, Container, ListGroup, Button
   } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Chatrooms=()=>{
     const [chatrooms, setChatrooms]= useState([])
     const [collaspeChatRoom, setCollaspeChatRoom]= useState([true])
-    let test=true
-    const getWindowWidth=()=>{
-        const {innerWidth: width}= window;
-        return(
-          width
-        )
-      }
-      const [windowWidth, setWindowWidth] =useState(getWindowWidth)
-      useEffect(()=>{
-        setWindowWidth(getWindowWidth())
-      })
-
-    const openMenu=()=>{
-        setCollaspeChatRoom(false)
-    }
-    // const collaspedChat=()=>(
-
-    // )
-
+ 
     useEffect(()=>{
         fetch('/getChatRoomList').then(res=>{
            return res.json()
@@ -36,15 +16,9 @@ const Chatrooms=()=>{
             setChatrooms(data)
         })
     },[])
-    if(windowWidth>=1300){
     return(
         <>
-        <Card className='sideBar' style={{
-            // width: '15rem', 
-            // height:'100rem',
-            // padding:'0',
-            
-            }}>
+        <Card className='sideBar'>
             <Card.Title className='text-center fs-3 border-bottom border-dark'>
                 ChatRooms
             </Card.Title>
@@ -86,23 +60,7 @@ const Chatrooms=()=>{
         </Card>
         </>
     )
-    }
-    else return(
-        <>
-            {/* <Button className='hamMen' onClick={openMenu} >
 
-            </Button> */}
-                <span className='hamMen'>
-                    <FontAwesomeIcon
-                    icon={faBars}
-                    size='3x'
-                    onClick={openMenu}
-                    />
-                </span>
-                {collaspeChatRoom}
-
-        </>
-    )
 }
 
 export default Chatrooms
--- client/src/Components/ChatroomIndex.js
+++ client/src/Components/ChatroomIndex.js
@@ -1,11 +1,57 @@
 import Chatrooms from "./Chatroom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
+
 const ChatRoomIndex=()=>{
 
-    return(
-        <>
-        <Chatrooms/>
+    const [collaspeChatRoom, setCollaspeChatRoom]= useState(['closed'])
+    const openMenu=()=>{
+        setCollaspeChatRoom('open')
+    }
+    const getWindowWidth=()=>{
+        const {innerWidth: width}= window;
+        return(
+          width
+        )
+      }
+      const [windowWidth, setWindowWidth] =useState(getWindowWidth)
+      useEffect(()=>{
+        setWindowWidth(getWindowWidth())
+      })
+    if (windowWidth>=1400){
+        return(
+            <>
+            <Chatrooms/>
+            </>
+        )
+
+    }
+    else return(
+<>
+            {/* <Button className='hamMen' onClick={openMenu} >
+
+            </Button> */}
+                <span className='hamMen'>
+                    <FontAwesomeIcon
+                    icon={faBars}
+                    size='3x'
+                    onClick={openMenu}
+                    />
+                </span>
+                {(()=>{
+                    console.log('ye')
+                    if(collaspeChatRoom==='open'){
+                        return(
+                            <Chatrooms className='transparentChatRoom' />
+                        )
+                    }
+                })()}
+
         </>
+      
     )
 }
 
 export default ChatRoomIndex
+
