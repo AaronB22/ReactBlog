83288b189bd85627090e20dcf67c61a3bdfe70c2
Aaron Bailey <aaronthesnowboarder@live.com>
2022-03-03 22:51:40 -0800

    change how frontend is serve

--- client/src/utils/SocketProvider.js
+++ client/src/utils/SocketProvider.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {io} from 'socket.io-client'
-const PORT = 'http://localhost:3001'
 
-export const socket = io(PORT)
+export const socket = io()
 export const SocketContext = React.createContext(socket)
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const root = require('path').join(__dirname, 'build')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const io = require('socket.io', {
-  rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling']
-})(3001, {
-  cors: {
-    origin: ['http://localhost:3000', 'http://localhost:5000']
-  }
-})
+const path= require('path')
+// const server= http.createServer()
+// const io = require('socket.io', {
+//   rememberTransport: false, transports: ['WebSocket', 'Flash Socket', 'AJAX long-polling']
+// })(3001, {
+//   cors: {
+//     origin: ['http://localhost:3000', 'http://localhost:5000']
+//   }
+// })
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,6 +19,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+// app.use(express.static)
+
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reactBlogDB',  {
@@ -24,6 +29,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reactBlogDB',  
 
 app.use(require("./database/routesdb"))
 const User=require('./database/models/User')
+const http= require('http')
+const server= http.createServer(app)
+const { Server } = require("socket.io");
+const io = new Server(server);
 io.on("connection", socket =>{
   console.log('USER CONNECTED')
   socket.on('send-msg', (msgObj)=>{
@@ -41,6 +50,17 @@ io.on("connection", socket =>{
   })
 })
 
+
+app.use(express.static(root));
+
+app.get('/test', (req, res)=>{
+  // console.log(io)
+  console.log(root)
+  // res.json(io)
+})
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
