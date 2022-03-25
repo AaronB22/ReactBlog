38d7aa2ca2b9394c46e197c6f1d6dc6bccd473ac
Aaron Bailey <aaronthesnowboarder@live.com>
2022-03-25 10:05:42 -0700

    change socket url to work with heroku

--- client/src/utils/SocketProvider.js
+++ client/src/utils/SocketProvider.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {io} from 'socket.io-client'
 
-console.log(window.location)
-export const socket = io(`http://${window.location.hostname}:8900`)
+export const socket = io(`${window.location.protocol}//${window.location.hostname}:8900`)
 export const SocketContext = React.createContext(socket)
