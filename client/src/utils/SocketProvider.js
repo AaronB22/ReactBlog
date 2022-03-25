36c5752491d702604d846a556387a5579a0405f7
Aaron Bailey <aaronthesnowboarder@live.com>
2022-03-25 10:00:16 -0700

    heroku socket test

--- client/src/utils/SocketProvider.js
+++ client/src/utils/SocketProvider.js
@@ -1,4 +1,6 @@
 import React from "react";
 import {io} from 'socket.io-client'
+
+console.log(window.location)
 export const socket = io(`http://${window.location.hostname}:8900`)
 export const SocketContext = React.createContext(socket)
